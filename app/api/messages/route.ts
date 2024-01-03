560774bd2baca785e525ea5edd32246f19132c4a
Your New Username <adarshj031@gmail.com>
2024-01-03 18:50:56 +0530

    chat-scroll hook

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,20 +1,25 @@
+import { NextResponse } from "next/server";
+import { Message } from "@prisma/client";
+
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { Message } from "@prisma/client";
-import { NextResponse } from "next/server";
+
 const MESSAGES_BATCH = 10;
+
 export async function GET(req: Request) {
   try {
     const profile = await currentProfile();
     const { searchParams } = new URL(req.url);
+
     const cursor = searchParams.get("cursor");
     const channelId = searchParams.get("channelId");
 
     if (!profile) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
+
     if (!channelId) {
-      return new NextResponse("ChannelId missing", { status: 400 });
+      return new NextResponse("Channel ID missing", { status: 400 });
     }
 
     let messages: Message[] = [];
@@ -67,10 +72,10 @@ export async function GET(req: Request) {
 
     return NextResponse.json({
       items: messages,
-      cursor: nextCursor,
+      nextCursor,
     });
   } catch (error) {
-    console.log("[MESSAGE_GET]", error);
+    console.log("[MESSAGES_GET]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
